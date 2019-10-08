ba78297a5f0d30a7c38e677b4db7ff3ade5889c9
simranbal04 <simranbal01@gmail.com>
2019-10-08 16:35:40 -0400

    Iniial commit, Added enemy file

---
+++ EnemyPixel.java
@@ -0,0 +1,61 @@
+package com.example.android_2d_chippy;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Rect;
+
+public class EnemyPixel {
+    private Bitmap image;
+    private Rect hitbox;
+
+    private int xPosition;
+    private int yPosition;
+
+
+    public EnemyPixel(Context context, int xPosition, int yPosition) {
+        this.xPosition = xPosition;
+        this.yPosition = yPosition;
+        // 2. Set the default image - all enemies have same image
+        this.image = BitmapFactory.decodeResource(context.getResources(),R.drawable.architectonic);
+
+        this.hitbox = new Rect(
+                this.xPosition,
+                this.yPosition,
+                this.xPosition + this.image.getWidth(),
+                this.yPosition + this.image.getHeight()
+        );
+    }
+
+    public Bitmap getImage() {
+        return image;
+    }
+
+    public void setImage(Bitmap image) {
+        this.image = image;
+    }
+
+    public Rect getHitbox() {
+        return hitbox;
+    }
+
+    public void setHitbox(Rect hitbox) {
+        this.hitbox = hitbox;
+    }
+
+    public int getxPosition() {
+        return xPosition;
+    }
+
+    public void setxPosition(int xPosition) {
+        this.xPosition = xPosition;
+    }
+
+    public int getyPosition() {
+        return yPosition;
+    }
+
+    public void setyPosition(int yPosition) {
+        this.yPosition = yPosition;
+    }
+}
---
+++ GameEngine.java
@@ -0,0 +1,4 @@
+package com.example.android_2d_chippy;
+
+public class GameEngine {
+}
